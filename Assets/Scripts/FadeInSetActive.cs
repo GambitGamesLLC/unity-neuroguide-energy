035a817845581cbd08e613219f13e5ed04c3f41c
corysmith19x <110064828+corysmith19x@users.noreply.github.com>
2025-07-17 14:45:17 -0400

    UI updated, fade in/out screen updated

--- Assets/Scripts/FadeInSetActive.cs
+++ Assets/Scripts/FadeInSetActive.cs
@@ -4,18 +4,31 @@ using System.Collections;
 public class FadeInSetActive : MonoBehaviour
 {
 
-    public GameObject fadeScreen;
+    public GameObject fadeInScreen;
+    public GameObject fadeOutScreen;
+
+    public bool fadeIn;
+    public bool fadeOut;
+    public bool waiting;
+    public bool fadeInMenu;
+
+    public float timeSeconds = 3f;
 
     void Start()
     {
+        fadeIn = true;
+        waiting = false;
 
-        if (fadeScreen != null)
-        {
-            fadeScreen.SetActive(true);
-
-        }
+        FadeIn();
 
     }
+
+    private void FixedUpdate()
+    {
+        FadeIn();
+        FadeOut();
+    }
+
     public void DelayedAction()
     {
         StartCoroutine(ExecuteAfterDelay());
@@ -26,15 +39,78 @@ public class FadeInSetActive : MonoBehaviour
         //Debug.Log("Starting delay...");
 
         // Wait for 3 seconds using WaitForSeconds
-        yield return new WaitForSeconds(3f);
+        if (fadeInMenu == true)
+        {
+            timeSeconds = 2.5f;
+        }
+        else
+        {
+            timeSeconds = 3f;
+        }
+
+        if (waiting == false)
+        {
+            waiting = true;
+            yield return new WaitForSeconds(3f);
+        }
+        
+        
 
         //Debug.Log("3 seconds have passed! Executing action now.");
         // Put the code you want to execute after the delay here
-        fadeScreen.SetActive(false);
+        if(fadeIn == true)
+        {
+            fadeInScreen.SetActive(false);
+            fadeIn = false;
+        }
+        if(fadeOut == true)
+        {
+
+            fadeOutScreen.SetActive(false);
+            fadeOut = false;
+            
+        }
+        if(fadeInMenu == true)
+        {
+            fadeInScreen.SetActive(false);
+            fadeInMenu = false;
+        }
+
+        timeSeconds = 3f;
+        waiting = false;
     }
 
     private void Awake()
     {
         DelayedAction();
+    }
+
+    public void FadeIn()
+    {
+        
+        if (fadeInScreen != null && fadeIn == true && waiting == false)
+        {
+            //waiting = false;
+            fadeInScreen.SetActive(true);
+            DelayedAction();
+
+        }
+        if (fadeInScreen != null && fadeInMenu == true)
+        {
+            fadeInScreen.SetActive(true);
+            DelayedAction();
+        }
+    }
+
+    public void FadeOut()
+    {
+
+        if (fadeOutScreen != null && fadeOut == true && waiting == false)
+        {
+            //waiting = false;
+            fadeOutScreen.SetActive(true);
+            DelayedAction();
+
+        }
     }
 }
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -2,11 +2,17 @@ using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.Collections;
 
 public class UIScript : MonoBehaviour
 {
     [SerializeField] UIDocument uiDocument;
+    [SerializeField] FadeInSetActive fadeInOutScript;
     private VisualElement root;
+    private VisualElement mainMenu;
+    private VisualElement selectionMenu;
 
 
     void Start()
@@ -16,10 +22,19 @@ public class UIScript : MonoBehaviour
         var settingsBtn = root.Q<VisualElement>("SettingsButton");
         var startBtn = root.Q<VisualElement>("TextContainer");
         var quitBtn = root.Q<VisualElement>("ExitButton");
+        var exp1Btn = root.Q<VisualElement>("Exp1");
+        var exp2Btn = root.Q<VisualElement>("Exp2");
 
         settingsBtn.RegisterCallback<ClickEvent>(OnSettingsEvent);
         startBtn.RegisterCallback<ClickEvent>(OnAnywhereEvent);
         quitBtn.RegisterCallback<ClickEvent>(OnQuitEvent);
+        exp1Btn.RegisterCallback<ClickEvent>(OnExperience1);
+        exp2Btn.RegisterCallback<ClickEvent>(OnExperience2);
+
+        mainMenu = root.Q<VisualElement>("MainMenuContainer");
+        selectionMenu = root.Q<VisualElement>("SelectionContainer");
+
+        selectionMenu.SetEnabled(false);
     }
 
 
@@ -32,10 +47,65 @@ public class UIScript : MonoBehaviour
     {
         Debug.Log("Quit Button Clicked");
         Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
-    
+
     private void OnAnywhereEvent(ClickEvent evt)
     {
         Debug.Log("Anywhere Clicked");
+        fadeInOutScript.fadeOut = true;
+        DelayedAction();
+    }
+
+    private void OnExperience1(ClickEvent evt)
+    {
+        Debug.Log("Experience 1 Clicked");
+    }
+    private void OnExperience2(ClickEvent evt)
+    {
+        Debug.Log("Experience 2 Clicked");
+        DelayedAction2();
+    }
+
+    public void DelayedAction()
+    {
+        StartCoroutine(ExecuteAfterDelay());
+    }
+
+    IEnumerator ExecuteAfterDelay()
+    {
+        //Debug.Log("Starting delay...");
+
+        // Wait for 3 seconds using WaitForSeconds
+        yield return new WaitForSeconds(2.5f);
+
+        //SceneManager.LoadScene("Experience 2 - M3D");
+        fadeInOutScript.fadeInScreen.SetActive(true);
+
+        selectionMenu.style.display = DisplayStyle.Flex;
+        selectionMenu.style.visibility = Visibility.Visible;
+        selectionMenu.SetEnabled(true);
+
+        mainMenu.style.display = DisplayStyle.None;
+        mainMenu.style.visibility = Visibility.Hidden;
+        mainMenu.SetEnabled(false);
+
+        yield return new WaitForSeconds(2.5f);
+        fadeInOutScript.fadeInScreen.SetActive(false);
+
+
+    }
+    public void DelayedAction2()
+    {
+        StartCoroutine(ExecuteAfterDelay2());
+    }
+
+    IEnumerator ExecuteAfterDelay2()
+    {
+        fadeInOutScript.fadeOutScreen.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("Experience 2 - M3D");
     }
 }
